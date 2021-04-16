0cf00cf0a84098a8d247ec7cae9eec7761c0d475
RhymeWriter <maxi2ua@gmail.com>
2021-04-16 23:46:20 +0300

    Fourth commit

--- 1_homwork_1/JS/3_oneDemesionalArrays.js
+++ 1_homwork_1/JS/3_oneDemesionalArrays.js
@@ -16,7 +16,7 @@ console.log(getMinEl([5, 7, 2, 4]));
 
 console.log('2. Найти максимальный элемент массива');
 
-function getMaxElement(arr) {
+function getMaxEl(arr) {
     var max = arr[0];
     for (var i = 0; i < arr.length; i++) {
         if (max < arr[i]) {
@@ -26,7 +26,11 @@ function getMaxElement(arr) {
     return max;
 }
 
-function getIndexMinElement(arr) {
+console.log(getMaxEl([5, 7, 2, 4]));
+
+console.log('3. Найти индекс минимального элемента массива');
+
+function getIndexMinEl(arr) {
     var minIndex = 0;
     for (var i = 0; i < arr.length; i++) {
         if (arr[minIndex] > arr[i]) {
@@ -36,7 +40,11 @@ function getIndexMinElement(arr) {
     return minIndex;
 }
 
-function getIndexMaxElement(arr) {
+console.log(getIndexMinEl([5, 7, 2, 4]));
+
+console.log('4. Найти индекс максимального элемента массива');
+
+function getIndexMaxEl(arr) {
     var maxIndex = 0;
     for (var i = 0; i < arr.length; i++) {
         if (arr[maxIndex] < arr[i]) {
@@ -45,6 +53,10 @@ function getIndexMaxElement(arr) {
     }
     return maxIndex;
 }
+
+console.log(getIndexMaxEl([5, 7, 2, 4]));
+
+console.log('5. Посчитать сумму элементов массива с нечетными индексами');
 
 function getSumOddIndex(arr) {
     var sum = 0;
@@ -56,82 +68,4 @@ function getSumOddIndex(arr) {
     return sum;
 }
 
-function reverseArray(arr) {
-    var NewArr = [];
-    for (var i = arr.length - 1; i >= 0; i--) {
-        NewArr.push(arr[i]);
-    }
-    return NewArr;
-}
-
-function getSumOddElements(arr) {
-    var count = 0;
-    for (var i = 0; i < arr.length; i++) {
-        if (arr[i] % 2 !== 0) {
-            count++;
-        }
-    }
-    return count;
-}
-
-function changePlaceElements(arr) {
-    var temp = arr.splice(0, arr.length / 2);
-    var arr1 = arr.concat(temp);
-    return arr1;
-}
-
-function sortArrByBuble(arr) {
-    var temp;
-    for (var k = 0; k < arr.length; k++) {
-        var change = 0;
-        for (var i = 0; i < arr.length - 1 - k; i++) {
-            if (arr[i] > arr[i + 1]) {
-                change++;
-                temp = arr[i];
-                arr[i] = arr[i + 1];
-                arr[i + 1] = temp;
-            }
-        }
-        if (!change) {
-            break;
-        }
-    }
-
-    return arr;
-}
-
-function sortArrBySelect(arr) {
-    var temp;
-    for (var i = 0; i < arr.length; i++) {
-        var minEl = i;
-        var change = 0;
-        for (var k = i + 1; k < arr.length; k++) {
-            if (arr[k] < arr[minEl]) {
-                minEl = k;
-                change++;
-            }
-        }
-        if (minEl !== i) {
-            temp = arr[i];
-            arr[i] = arr[minEl];
-            arr[minEl] = temp;
-        }
-        if (!change) {
-            break;
-        }
-    }
-    return arr;
-}
-
-function sortArrByInsert(arr) {
-    for (var i = 1; i < arr.length; i++) {
-        var insertEl = arr[i];
-        var j = i - 1;
-        while (j >= 0 && arr[j] > insertEl) {
-            arr[j + 1] = arr[j];
-            j--;
-        }
-        arr[j + 1] = insertEl;
-    }
-    return arr;
-}
+console.log(getSumOddIndex([5, 7, 2, 4]));
