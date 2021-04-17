b0f9dec74e37e8c44d98642ba2ab377a12052a6b
RhymeWriter <maxi2ua@gmail.com>
2021-04-17 03:43:16 +0300

    Six commit - 100%

--- 2_homework_stars/JS/stars.js
+++ 2_homework_stars/JS/stars.js
@@ -100,3 +100,54 @@ function star6() {
 }
 
 console.log(star6());
+
+function star7() {
+    var star = '';
+    for (var i = 1; i < 8; i++) {
+        for (var j = 7, k = 1; j >= 1, k < 8; j--, k++) {
+            if (i === j || k === i) {
+                star += '* ';
+            } else {
+                star += '  ';
+            }
+        }
+        star += "\n";
+    }
+    return star;
+}
+
+console.log(star7());
+
+function star8() {
+    var star = '';
+    for (var i = 1; i < 8; i++) {
+        for (var j = 7, k = 1; j >= 1, k < 8; j--, k++) {
+            if ((i === 1 || i === j || k === i) && i < 5) {
+                star += '* ';
+            } else {
+                star += '  ';
+            }
+        }
+        star += "\n";
+    }
+    return star;
+}
+
+console.log(star8());
+
+function star9() {
+    var star = '';
+    for (var i = 1; i < 8; i++) {
+        for (var j = 7, k = 1; j >= 1, k < 8; j--, k++) {
+            if ((i === 7 || i === j || k === i) && i > 3) {
+                star += '* ';
+            } else {
+                star += '  ';
+            }
+        }
+        star += "\n";
+    }
+    return star;
+}
+
+console.log(star9());
